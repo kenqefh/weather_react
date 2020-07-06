51789ed65cb35d8eef740b7a17766b8709b868fa
Elvis Frank Dominguez Vidal <elvisf.dominguez@gmail.com>
2020-07-06 17:52:25 -0500

    plan de transformacion

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ForecastItem from './ForecastItem'
+import transformForecast from './../service/transformForecast'
 import './styles.css'
-
+/*
 const days = [
     'Lunes',
     'Martes',
@@ -13,17 +14,52 @@ const days = [
     'Domingo',
 ];
 
+const data = {
+    temperature: 10,
+    humidity: 10,
+    weatherState: 'normal',
+    wind: 'normal',
+}
+*/
+const api_key = '3a3d4bc5f3abd1fa7a9b140209d7fbc4'
+const url = 'api.openweathermap.org/data/2.5/forecast'
+//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
 class ForecastExtended extends Component {
+    constructor() {
+        super()
+        this.state = { forecastData: null }
+    }
+
+    componentDidMount() {
+        const url_forecast = `${url}?q=${this.props.city}&appid=${api_key}`
+
+        fetch(url_forecast).then(
+            data => (data.json())
+        ).then(
+            weather_data => {
+                console.log(weather_data);
+                const forecastData = transformForecast(weather_data);
+                this.setState({ forecastData })
+            }
+        );
+    }
+
     renderForecastItemDays() {
-        return days.map(day => (<ForecastItem weekDay={day} key={day} />))
+        return <h2>render Items</h2>
+        //return days.map(day => (<ForecastItem weekDay={day} key={day} hour={11} data={data} />))
+    }
+
+    renderProgress() {
+        return <h4>Cargando pronostico extendido</h4>
     }
 
     render() {
         const { city } = this.props
+        const { forecastData } = this.state
         return (
             <div>
                 <h2 className='forecast-title'>Pronostico extendido para {city}</h2>
-                {this.renderForecastItemDays()}
+                {forecastData ? this.renderForecastItemDays() : this.renderProgress()}
             </div>
         )
     }
--- src/components/ForecastItem/index.js
+++ src/components/ForecastItem/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-const ForecastItem = ({ weekDay }) => (
-    <div>{weekDay}</div>
+import WeatherData from './../WeatherLocation/WeatherData'
+
+const ForecastItem = ({ weekDay, hour, data }) => (
+    <div>
+        <div>{weekDay} Hora: {hour} hs.</div>
+        <WeatherData data={data} />
+    </div>
 );
 
 ForecastItem.propTypes = {
     weekDay: PropTypes.string.isRequired,
+    hour: PropTypes.number.isRequired,
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    }),
 }
 
 export default ForecastItem
